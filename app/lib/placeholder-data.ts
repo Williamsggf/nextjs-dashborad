287242a7543b0acd6ab11f7a9bb9a94f33f92bc5
unknown <williamsggf@hotmail.com>
2025-04-15 16:18:30 -0300

    Ajuste estrutura do banco de dados

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,6 +1,7 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
-const users = [
+
+/* const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
@@ -145,3 +146,4 @@ const revenue = [
 ];
 
 export { users, customers, invoices, revenue };
+ */
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,11 +1,12 @@
 import bcrypt from "bcryptjs";
 import postgres from "postgres";
-import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function seedUsers() {
   await sql`
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -13,33 +14,83 @@ async function seedUsers() {
       password TEXT NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      user_status BOOLEAN DEFAULT true
+      is_active BOOLEAN DEFAULT true
     );
   `;
+}
 
+async function seedCompanies() {
   await sql`
-    ALTER TABLE users 
-    ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-    ADD COLUMN IF NOT EXISTS user_status BOOLEAN DEFAULT true;
+    CREATE TABLE IF NOT EXISTS companies (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      cnpj VARCHAR(14) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
   `;
+}
 
-  const insertedUsers = await Promise.all(
-    users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
-      return sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-    })
-  );
+async function seedAddresses() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS addresses (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      company_id UUID REFERENCES companies(id) NOT NULL,
+      street VARCHAR(255) NOT NULL,
+      number VARCHAR(9) NOT NULL,
+      complement VARCHAR(255),
+      district VARCHAR(255) NOT NULL,
+      city VARCHAR(255) NOT NULL,
+      state VARCHAR(2) NOT NULL,
+      zip_code VARCHAR(9) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+}
 
-  return insertedUsers;
+async function seedDepartments() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS departments (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      company_id UUID REFERENCES companies(id) NOT NULL,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      name VARCHAR(100) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+}
+
+async function seedStaff() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS staff (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      department_id UUID REFERENCES departments(id) NOT NULL,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+}
+
+async function seedTimeLogs() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS time_logs (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      type VARCHAR(20) NOT NULL CHECK (type IN (
+        'check_in',
+        'break_start',
+        'break_end',
+        'check_out'
+      )),
+      date DATE NOT NULL,
+      time TIME NOT NULL,
+      notes TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -50,79 +101,45 @@ async function seedInvoices() {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `;
-
-  const insertedInvoices = await Promise.all(
-    invoices.map(
-      (invoice) => sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `
-    )
-  );
-
-  return insertedInvoices;
 }
 
 async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) NOT NULL,
-      image_url VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `;
-
-  const insertedCustomers = await Promise.all(
-    customers.map(
-      (customer) => sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `
-    )
-  );
-
-  return insertedCustomers;
 }
 
 async function seedRevenue() {
   await sql`
     CREATE TABLE IF NOT EXISTS revenue (
-      month VARCHAR(4) NOT NULL UNIQUE,
-      revenue INT NOT NULL,
+      month VARCHAR(7) NOT NULL UNIQUE, -- ex: '2025-04'
+      amount INT NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `;
-
-  const insertedRevenue = await Promise.all(
-    revenue.map(
-      (rev) => sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `
-    )
-  );
-
-  return insertedRevenue;
 }
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+/*     await seedUsers();
+    await seedCompanies();
+    await seedAddresses(); */
+    await seedDepartments();
+    await seedStaff();
+    await seedTimeLogs();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
 
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
+    console.error("Seed error:", error);
     return Response.json({ error }, { status: 500 });
   }
 }
